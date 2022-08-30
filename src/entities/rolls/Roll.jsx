53313be129fb739870e7c6b0676de2d35f1c50ab
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-08-30 23:56:15 +0300

    add price block + some design fixes

--- src/entities/rolls/Roll.jsx
+++ src/entities/rolls/Roll.jsx
@@ -5,27 +5,43 @@ import closedImage from "../../shared/img/roll-outside/closed-roll.svg";
 const Roll = ({roll}) => {
 
   const Open = () => {
-    return <img src={openImage} alt="open rolls and sushi" className="open"/>
+    return <img src={openImage} alt="open roll" className="roll-ingredient__circle"/>
   }
   const Closed = () => {
-    return <img src={closedImage} alt="closed rolls and sushi" className="open"/>
+    return <img src={closedImage} alt="closed roll" className="roll-ingredient__circle"/>
   }
-
   return (
     <div className="roll">
       <div className="roll-wrapper">
         <h3 className="roll__header">{roll.name}</h3>
         <div className="roll-recipe">
-          {roll.open
-            ? <Open/>
-            : <Closed/>
-          }
-          {
-            roll.inside.map((ingredient, index) => {
-              return <img key={index} src={ingredient} alt="ingredient"/>
-            })
-          }
+            {roll.outside &&
+              <img
+                src={roll.outside}
+                alt="outside ingredient"
+                className="roll-ingredient roll-ingredient--outside"
+              />
+            }
+            {roll.open
+              ? <Open/>
+              : <Closed/>
+            }
+            {
+              roll.inside.map((ingredient, index) => {
+                return (
+                  <img
+                    key={index}
+                    src={ingredient}
+                    alt="inside ingredient"
+                    className="roll-ingredient roll-ingredient--inside"
+                  />
+                )
+              })
+            }
         </div>
+        <h3 className="roll__price">
+          300 ₽
+        </h3>
       </div>
       <img src={roll.image} alt="roll" className="roll__image"/>
     </div>
