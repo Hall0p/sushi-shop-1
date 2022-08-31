a76a42adb633a55547d6835fce4562bcc5dd7420
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-08-31 07:10:03 +0300

    create footer

---
+++ src/widgets/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <div className="footer">
+        <span className="footer__text">Panda &lt;3</span>
+        <h4 className="footer__date">2022</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
