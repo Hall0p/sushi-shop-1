0177aa0bbfee55f2e3f60771577be212099f7616
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-09-03 14:38:20 +0300

    some set changes

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,10 +2,12 @@ import RollList from "../pages/RollList";
 import './index.scss'
 import Header from "../widgets/Header";
 import Footer from "../widgets/Footer";
+import Switcher from "../entities/switcher/Switcher";
 
 function App() {
   return (
     <div className="app">
+      <Switcher/>
       <Header/>
       <RollList/>
       <Footer/>
---
+++ src/pages/Sets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sets = () => {
+  return (
+    <div className="main">
+      <div className="main-wrapper container">
+
+      </div>
+    </div>
+  );
+};
+
+export default Sets;
