30717582e42b9bd06d2afe9c753bb549fce3e9bf
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-09-09 18:29:48 +0300

    app rename

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Index from './app';
+import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Index/>
+    <App/>
   </React.StrictMode>
 );
