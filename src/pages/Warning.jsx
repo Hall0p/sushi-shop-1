89e833441c50ab61b8b193e18fbcfbd8a1037ac2
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-09-12 16:56:26 +0300

    create warning page

---
+++ src/pages/Warning.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Warning = () => {
+  return (
+    <div className="main">
+      <div className="main-wrapper container">
+        <h1>404 NO SUCH PAGE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Warning;
