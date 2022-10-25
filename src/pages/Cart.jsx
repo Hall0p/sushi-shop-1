ea7cc62dd2068426a04f1bce2494e4faa6f6c93d
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-10-25 17:41:34 +0300

    create cart structure

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,33 +9,49 @@ const Cart = () => {
       <div className="main-wrapper container">
         <div className="cart">
           <div className="cart-left">
-            <h2 className="cart__header">
+            <h1 className="cart__header">
               Моя корзина
-            </h2>
-            <div className="cart-wrapper">
-              {/*Сюда массив с содержимым корзины*/
-                <div className="cart__product"></div>}
+            </h1>
+            <div className="cart-products">
+                <div className="cart-item">
+                  <img src="#" alt="cart item" className="cart__image"/>
+                  <div className="cart-item-wrapper">
+                    <div className="cart-item__left">
+                      <h3 className="cart-item__header">Name</h3>
+                      <div className="cart-item__weight">250г.</div>
+                    </div>
+                    <div className="cart-item__right">
+                      <button className="cart-item__counter"></button>
+                      <span className="cart-item__price">300р.</span>
+                      <button className="cart-item__cancel"></button>
+                    </div>
+                  </div>
+                </div>
             </div>
           </div>
-          <div className="cart-right">
-            <div className="sticky"><div className="cart-center">
-              <ul className="cart-sum">
-                <li className="cart-sum--left">Товары:</li>
-                <li className="cart-sum--right">{sum}</li>
-              </ul>
-              <ul className="cart-sum">
-                <li className="cart-sum--left">Доставка:</li>
-                <li className="cart-sum--right color--green">бесплатно</li>
-              </ul>
-              <ul className="cart-sum cart-sum__total">
-                <li className="cart-sum--left">Итого</li>
-                <li className="cart-sum--right">{sum}</li>
-              </ul>
+          <div className="cart-right-wrapper">
+            <div className="sticky">
+              <div className="cart-right">
+                <div className="cart-center">
+                  <ul className="cart-sum">
+                    <li className="cart-sum--left">Товары:</li>
+                    <li className="cart-sum--right">{sum}</li>
+                  </ul>
+                  <ul className="cart-sum">
+                    <li className="cart-sum--left">Доставка:</li>
+                    <li className="cart-sum--right color--green">бесплатно</li>
+                  </ul>
+                  <ul className="cart-sum cart-sum__total">
+                    <li className="cart-sum--left">Итого</li>
+                    <li className="cart-sum--right">{sum}</li>
+                  </ul>
+                </div>
+                <div className="cart-right-bottom">
+                  <button className="cart__button cart__button--left">Самовывоз</button>
+                  <button className="cart__button cart__button--right">Доставка</button>
+                </div>
+              </div>
             </div>
-              <div className="cart-right-bottom">
-                <button className="cart__button cart__button--left">Самовывоз</button>
-                <button className="cart__button cart__button--right">Доставка</button>
-              </div></div>
           </div>
         </div>
       </div>
