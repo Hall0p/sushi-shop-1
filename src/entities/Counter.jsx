caf33ebd0bd5f5aa121d6840f6637725a7f3ffa8
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-10-27 18:52:42 +0300

    add counter bind to cart

--- src/entities/Counter.jsx
+++ src/entities/Counter.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import minus from '../shared/img/minus.svg'
 import plus from '../shared/img/plus.svg'
 
-const Counter = () => {
+const Counter = ({addAction, removeAction, count}) => {
+
   return (
     <div className="counter">
-      <button className="remove counter__button">
+      <button
+        onClick={() => removeAction()}
+        className="remove counter__button"
+      >
         <img className="counter__image" src={minus} alt=""/>
       </button>
-      <span className="count">1</span>
-      <button className="add counter__button">
+      <span className="count">
+        {count}
+      </span>
+      <button
+        onClick={() => addAction()}
+        className="add counter__button"
+      >
         <img className="counter__image" src={plus} alt=""/>
       </button>
     </div>
