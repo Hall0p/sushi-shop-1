edb10b3c54f8ba3668f4b4c93ad75175d521eee5
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-10-30 20:49:52 +0300

    fix cart reducer remove from cart

--- src/shared/store/cartReducer.js
+++ src/shared/store/cartReducer.js
@@ -9,31 +9,25 @@ const ADD_SET_CART = 'ADD_SET_CART'
 const REMOVE_SET_CART = 'REMOVE_SET_CART'
 
 export const cartReducer = (state = defaultState, action) => {
-  let isDeleted = false
-
   switch (action.type) {
     case ADD_ROLL_CART:
       return {...state, rolls: [...state.rolls, action.payload]}
-    case REMOVE_ROLL_CART:
-      isDeleted = false
-      return {...state, rolls:
-        state.rolls.filter((roll) => {
-          if (isDeleted) return true
-          isDeleted = true
-          return roll !== action.payload
-        })
-      }
+    case REMOVE_ROLL_CART: {
+      const copyRolls = state.rolls.slice()
+      const index = copyRolls.indexOf(action.payload)
+      copyRolls.splice(index,1)
+
+      return {...state, rolls: copyRolls}
+    }
     case ADD_SET_CART:
       return {...state, sets: [...state.sets, action.payload]}
-    case REMOVE_SET_CART:
-      isDeleted = false
-      return {...state, sets:
-          state.sets.filter((set) => {
-            if (isDeleted) return true
-            isDeleted = true
-            return set !== action.payload
-          })
-      }
+    case REMOVE_SET_CART: {
+      const copySets = state.sets.slice()
+      const index = copySets.indexOf(action.payload)
+      copySets.splice(index,1)
+
+      return {...state, sets: copySets}
+    }
     default:
       return state;
   }
